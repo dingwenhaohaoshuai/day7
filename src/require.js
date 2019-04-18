d173ec751b7ad0087f326e90918ab9cc3b3f9108
dwh <dwh1977607914@163.com>
2019-04-18 09:07:20 +0800

    day6

---
+++ src/require.js
@@ -0,0 +1,17 @@
+function requier(a, b) {
+    //return  抛出
+    var sum = a + b
+        //导入模块 require加上模块名
+        //引入js 配置路径 依赖  
+    return sum
+}
+
+function define() {
+    //定义模块
+    //初始化init 
+    //设置依赖 回调函数
+    return
+
+}
+requier()
+define()
